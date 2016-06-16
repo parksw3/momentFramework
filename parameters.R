c13c86379cf69cf5a6d35e96a714ccad0adbce2f
parksw3 <parksw3@mcmaster.ca>
2016-06-16 09:52:47 -0400

    .

--- parameters.R
+++ parameters.R
@@ -1,4 +1,17 @@
 parms.skeleton <- list(
+	N.diff = 0.01,
+	N.min = 0.01,
+	N.max = 80,
+	mean = 4,
+	sd = 5,
+	beta = 0.2,
+	mu = 0,
+	iniI = 0.001
+)
+
+class(parms.skeleton) <- c("list", "parlist")
+
+parms.skeleton2 <- list(
 	mean = 1,
 	beta = 0.8,
 	mu = 0,
